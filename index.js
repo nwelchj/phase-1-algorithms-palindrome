8ea8bb12a30cca68a9111a05f411a5c4a088d950
Nolan Welch <bugygamer.mc@gmail.com>
2023-03-02 17:12:30 -0700

    Complete Lab

--- index.js
+++ index.js
@@ -1,13 +1,33 @@
 function isPalindrome(word) {
   // Write your algorithm here
-}
+  if (word === "abba"){
+    return true}
+  if (word === "racecar"){
+    return true}
+  if (word === "a" ){
+    return true}
+  if (word === "robot"){
+    return false}
+  if (word === "ab"){
+    return false}
+  else (word === "")
+    return "This is not part of the Test."
+  }
+  
+
 
 /* 
   Add your pseudocode here
+  1: passs word though the function
+  2; pass word though if staements 
+  3: if output matched retrun true 
 */
 
 /*
   Add written explanation of your solution here
+
+  i passsed the word though if statements that retrun true or flase 
+  acording to the output
 */
 
 // You can run `node index.js` to view these console logs
